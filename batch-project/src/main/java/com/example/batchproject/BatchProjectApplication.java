36226867f085519a3e59a4d5fdc67a3e803b1fd5
Ferdian Novendra <ferdiannovendra15@gmail.com>
2022-06-08 16:34:24 +0700

    First

--- batch-project/src/main/java/com/example/batchproject/BatchProjectApplication.java
+++ batch-project/src/main/java/com/example/batchproject/BatchProjectApplication.java
@@ -2,9 +2,13 @@ package com.example.batchproject;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,7 +26,13 @@ public class BatchProjectApplication {
 
 	@Bean
 	public Step packageItemStep() {
-		return this.stepBuilderFactory.get("packageItemStep").tasklet()
+		return this.stepBuilderFactory.get("packageItemStep").tasklet(new Tasklet() {
+			@Override
+			public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+				System.out.println("The item has been packaged");
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
 	}
 
 	@Bean
