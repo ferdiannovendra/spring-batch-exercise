fe181ffde3656466c398cf666427a7ff5738581a
Ferdian Novendra <ferdiannovendra15@gmail.com>
2022-06-15 17:39:35 +0700

    Item Writng

--- batch-project-chunk/src/main/java/com/example/batchproject/BatchProjectApplication.java
+++ batch-project-chunk/src/main/java/com/example/batchproject/BatchProjectApplication.java
@@ -1,5 +1,7 @@
 package com.example.batchproject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
@@ -15,47 +17,136 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
+import javax.sql.DataSource;
+import java.io.File;
 import java.util.List;
 
 @SpringBootApplication
 @EnableBatchProcessing
 public class BatchProjectApplication {
 
+	Logger logger = LoggerFactory.getLogger(getClass());
+	public static String[] names = new String[] {"orderId", "firstName", "lastName", "email", "cost", "itemId", "itemName", "shipDate"};
+
+	public static String ORDER_SQL = "select order_id, first_name, last_name, email, cost, item_id, item_name, ship_date "
+			+ "from SHIPPED_ORDER order by order_id";
 	@Autowired
 	public JobBuilderFactory jobBuilderFactory;
 
 	@Autowired
 	public StepBuilderFactory stepBuilderFactory;
 
+	@Autowired
+	public DataSource dataSource;
+
 	@Bean
-	public ItemReader<String> itemReader() {
-		return new SimpleItemReader();
+	public ItemWriter<Order> itemWriter() {
+		FlatFileItemWriter<Order> itemWriter = new FlatFileItemWriter<Order>();
+
+//		FileSystemResource classPathResource =
+
+		itemWriter.setResource(new FileSystemResource("/data/shipped_orders_output.csv"));
+		DelimitedLineAggregator<Order> aggregator = new DelimitedLineAggregator<Order>();
+		aggregator.setDelimiter(",");
+
+		BeanWrapperFieldExtractor<Order> fieldExtractor = new BeanWrapperFieldExtractor<Order>();
+		fieldExtractor.setNames(names);
+
+		aggregator.setFieldExtractor(fieldExtractor);
+
+		itemWriter.setLineAggregator(aggregator);
+		return itemWriter;
 	}
+
 	@Bean
-	public Step chunkBasedStep(){
+	public PagingQueryProvider queryProvider() throws Exception {
+		SqlPagingQueryProviderFactoryBean factory = new SqlPagingQueryProviderFactoryBean();
+		factory.setSelectClause("select order_id, first_name, last_name, email, cost, item_id, item_name, ship_date");
+		factory.setFromClause("from SHIPPED_ORDER");
+		factory.setSortKey("order_id");
+		factory.setDataSource(dataSource);
+		return factory.getObject();
+	}
+
+	//Multi Thread
+	@Bean
+	public ItemReader<Order> itemReader() throws Exception {
+		return new JdbcPagingItemReaderBuilder<Order>()
+				.dataSource(dataSource)
+				.name("jdbcCursorItemReader")
+				.queryProvider(queryProvider())
+				.rowMapper(new OrderRowMapper())
+				.pageSize(10)
+				.build();
+	}
+
+	//Single-thread
+//	@Bean
+//	public ItemReader<Order> itemReader() {
+//		return new JdbcCursorItemReaderBuilder<Order>()
+//				.dataSource(dataSource)
+//				.name("jdbcCursorItemReader")
+//				.sql(ORDER_SQL)
+//				.rowMapper(new OrderRowMapper())
+//				.build();
+//	}
+
+	//This code for read Item from CSV. If needed just uncomment this code below
+//	@Bean
+//	public ItemReader<Order> itemReader() {
+//		FlatFileItemReader<Order> itemReader = new FlatFileItemReader<>();
+//		itemReader.setLinesToSkip(1);
+//		ClassPathResource classPathResource = new ClassPathResource("data/shipped_orders.csv");
+//
+////		FileSystemResource fileSystemResource = new FileSystemResource("src/main/resources/data/shipped_orders.csv");
+//		itemReader.setResource(classPathResource);
+//
+//		System.out.println(classPathResource);
+//		System.out.println(classPathResource.exists());
+////		logger.info(fileSystemResource.toString());
+//
+//		DefaultLineMapper<Order> lineMapper = new DefaultLineMapper<>();
+//		DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+//		tokenizer.setNames(tokens);
+//
+//		lineMapper.setLineTokenizer(tokenizer);
+//
+//		lineMapper.setFieldSetMapper(new OrderFieldSetMapper());
+//
+//		itemReader.setLineMapper(lineMapper);
+//		return itemReader;
+//	}
+	@Bean
+	public Step chunkBasedStep() throws Exception {
 		return this.stepBuilderFactory.get("chunkBasedStep")
-				.<String, String>chunk(3)
+				.<Order, Order>chunk(10)
 				.reader(itemReader())
-				.writer(new ItemWriter<String>() {
-					@Override
-					public void write(List<? extends String> list) throws Exception {
-						System.out.println(String.format("Received list of size : %s", list.size()));
-						list.forEach(System.out::println);
-					}
-				}).build();
+				.writer(itemWriter()).build();
 	}
 
 
-
 	@Bean
-	public Job job(){
+	public Job job() throws Exception {
 		return this.jobBuilderFactory.get("job")
 				.start(chunkBasedStep())
 				.build();
---
+++ batch-project-chunk/src/main/java/com/example/batchproject/Order.java
@@ -0,0 +1,80 @@
+package com.example.batchproject;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Order {
+
+    private Integer orderId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private BigDecimal cost;
+    private String itemId;
+    private String itemName;
+    private Date shipDate;
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public BigDecimal getCost() {
+        return cost;
+    }
+
+    public void setCost(BigDecimal cost) {
+        this.cost = cost;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Date getShipDate() {
+        return shipDate;
+    }
+
+    public void setShipDate(Date shipDate) {
+        this.shipDate = shipDate;
+    }
+}
